35b802046855c6fbe8d9d71eae0701063d3ef427
Eduardo <eduardomndez@ymail.com>
2020-03-10 22:23:13 -0600

    Initial Design

---
+++ app/src/main/java/com/example/schoolagenda/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.schoolagenda;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalculatorActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/example/schoolagenda/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.schoolagenda;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
