3d7a94def7c5be38a8cf866b65a5314cd368c771
Luis Garcia <a01540063@itesm.mx>
2020-06-09 18:07:53 -0500

    calendar evenst

--- app/src/main/java/com/example/schoolagenda/AgendaActivity.java
+++ app/src/main/java/com/example/schoolagenda/AgendaActivity.java
@@ -1,8 +1,12 @@
 package com.example.schoolagenda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class AgendaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+    }
+    public void AddCalendarEvent(View view) {
+        Calendar calendarEvent = Calendar.getInstance();
+        Intent i = new Intent(Intent.ACTION_EDIT);
+        i.setType("vnd.android.cursor.item/event");
+        i.putExtra("beginTime", calendarEvent.getTimeInMillis());
+        i.putExtra("allDay", true);
+        i.putExtra("rule", "FREQ=YEARLY");
+        i.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000);
+        i.putExtra("title", "Calendar Event");
+        startActivity(i);
     }
 }
